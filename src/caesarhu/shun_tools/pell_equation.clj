4ff1a4553f9f7400024eff81a716b0621e91bdac
caesarhu <hupeishun@gmail.com>
2021-05-28 11:39:10 +0800

    update pell-equation

--- src/caesarhu/shun_tools/pell_equation.clj
+++ src/caesarhu/shun_tools/pell_equation.clj
@@ -15,15 +15,15 @@
                                  (loop [h2 0, h1 1,
                                         k2 1, k1 0,
                                         as as, n 0]
-                                   (if (and (>= n 1) (= r (- (*' h1 h1) (*' d k1 k1))))
+                                   (if (and (>= n 1) (= r (-' (*' h1 h1) (*' d k1 k1))))
                                      [h1 k1]
-                                     (recur h1 (+ (*' (first as) h1) h2)
-                                            k1 (+ (*' (first as) k1) k2)
+                                     (recur h1 (+' (*' (first as) h1) h2)
+                                            k1 (+' (*' (first as) k1) k2)
                                             (rest as) (inc n)))))]
-       (if (pos? r)
-         (continued-fractions)
-         (when (even? (count cf))
-           (continued-fractions))))))
+       (cond
+         (= 1 r) (continued-fractions)
+         (= -1 r) (when (even? (count cf))
+                    (continued-fractions))))))
   ([d]
    (find-fundamental-solution d 1)))
 
@@ -31,8 +31,8 @@
   ([d r]
    (when-let [[x y] (find-fundamental-solution d r)]
      (let [prod (fn [[xk yk]]
-                  [(+ (*' x xk) (*' d y yk))
-                   (+ (*' x yk) (*' y xk))])
+                  [(+' (*' x xk) (*' d y yk))
+                   (+' (*' x yk) (*' y xk))])
            solutions (iterate prod [x y])]
        (cond
          (= 1 r) solutions
