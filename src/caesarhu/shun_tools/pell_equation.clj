d2fc133c054b5057d9f91833e8cb9ec1ea3f39f6
caesarhu <hupeishun@gmail.com>
2021-05-28 09:25:04 +0800

    update pell-equation

--- src/caesarhu/shun_tools/pell_equation.clj
+++ src/caesarhu/shun_tools/pell_equation.clj
@@ -5,21 +5,25 @@
 (defn find-fundamental-solution
   "find the fundamental solution of pell's equation,  x^2 - dy^2 = 1 or -1"
   ([d r]
-   (cond
-     (not (pos-int? d)) nil
-     (misc/is-square? d) nil
-     (not (#{1 -1} r)) nil
-     (and (= -1 r) (odd? (count (misc/expand-continued-fraction d)))) nil
-     :else (let [cf (misc/expand-continued-fraction d)
-                 as (lazy-cat cf (cycle (rest cf)))]
-             (loop [h2 0, h1 1,
-                    k2 1, k1 0,
-                    as as, n 0]
-               (if (and (>= n 1) (= r (- (*' h1 h1) (*' d k1 k1))))
-                 [h1 k1]
-                 (recur h1 (+ (*' (first as) h1) h2)
-                        k1 (+ (*' (first as) k1) k2)
-                        (rest as) (inc n)))))))
+   (when (and (and (integer? d) (integer? r))
+              (pos? d)
+              (not (or (zero? d) (zero? r)))
+              ((complement misc/is-square?) d))
+     (let [cf (misc/expand-continued-fraction d)
+           as (lazy-cat cf (cycle (rest cf)))
+           continued-fractions (fn []
+                                 (loop [h2 0, h1 1,
+                                        k2 1, k1 0,
+                                        as as, n 0]
+                                   (if (and (>= n 1) (= r (- (*' h1 h1) (*' d k1 k1))))
+                                     [h1 k1]
+                                     (recur h1 (+ (*' (first as) h1) h2)
+                                            k1 (+ (*' (first as) k1) k2)
+                                            (rest as) (inc n)))))]
+       (if (pos? r)
+         (continued-fractions)
+         (when (even? (count cf))
+           (continued-fractions))))))
   ([d]
    (find-fundamental-solution d 1)))
 
@@ -28,7 +32,11 @@
    (when-let [[x y] (find-fundamental-solution d r)]
      (let [prod (fn [[xk yk]]
                   [(+ (*' x xk) (*' d y yk))
-                   (+ (*' x yk) (*' y xk))])]
-       (iterate prod [x y]))))
+                   (+ (*' x yk) (*' y xk))])
+           solutions (iterate prod [x y])]
+       (if (pos? r)
+         solutions
+         (->> (partition 2 solutions)
+              (map first))))))
   ([d]
    (pell-solutions d 1)))
