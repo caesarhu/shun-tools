98675c1da0978793ab01dd0528a3cecff670bf79
caesarhu <hupeishun@gmail.com>
2021-11-19 13:57:19 +0800

    update primes

--- src/caesarhu/shun_tools/primes.clj
+++ src/caesarhu/shun_tools/primes.clj
@@ -7,6 +7,25 @@
   [a b]
   (= 1 (math/gcd a b)))
 
+(defn sieve [^long n]
+  (let [primes (boolean-array (inc n) true)
+        sqrt-n (int (Math/ceil (Math/sqrt n)))]
+    (if (< n 2)
+      '()
+      (loop [p 3]
+        (if (< sqrt-n p)
+          (concat '(2)
+                  (filter #(aget primes %)
+                          (range 3 (inc n) 2)))
+          (do
+            (when (aget primes p)
+              (loop [i (* p p)]
+                (if (<= i n)
+                  (do
+                    (aset primes i false)
+                    (recur (+ i p p))))))
+            (recur  (+ p 2))))))))
+
 (defn- test-prime
   "Determine if a number is prime by looping through divisors"
   [x]
