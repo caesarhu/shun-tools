c50ae103a976c8fb57264e13c5b12e94ec8251c3
caesarhu <hupeishun@gmail.com>
2021-11-25 17:00:39 +0800

    update or-tools

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,20 +1,24 @@
 (ns user
-  (:require [caesarhu.shun-tools.or-tools.zebra :as z])
+  (:require [caesarhu.shun-tools.or-tools.zebra :as z]
+            [caesarhu.shun-tools.or-tools.MyCpSolverSolutionCallback])
   (:import com.google.ortools.Loader
-           caesarhu.shun-tools.or-tools.MyCpSolverSolutionCallback
            [com.google.ortools.sat CpModel CpSolver CpSolverStatus IntVar CpSolverSolutionCallback]))
 
 (Loader/loadNativeLibraries)
 
 (defn count-solutions
   []
-  (let [model (CpModel.)
+  (let [counter (atom 0)
+        cb (proxy [CpSolverSolutionCallback] []
+             (onSolutionCallback []
+               (swap! counter inc)))
+        model (CpModel.)
         numVals 3
         x (.newIntVar model 0 (dec numVals) "x")
         y (.newIntVar model 0 (dec numVals) "y")
         z (.newIntVar model 0 (dec numVals) "z")
         _ (.addDifferent model x y)
-        cb (new MyCpSolverSolutionCallback (to-array [x y z]))
         solver (CpSolver.)]
     (.setEnumerateAllSolutions (.getParameters solver) true)
-    (.solve solver model cb)))
+    (.solve solver model cb)
+    @counter))
