22ac7a7f27ef45bb26c2afeb44d87707767e74bc
caesarhu <hupeishun@gmail.com>
2021-11-27 16:36:35 +0800

    update ortools.sat

---
+++ dev/src/queens.clj
@@ -0,0 +1,33 @@
+(ns queens
+  (:require [caesarhu.shun-tools.or-tools.sat :as sat]
+            [clojure.math.combinatorics :as combo])
+  (:import [com.google.ortools.sat CpModel CpSolver CpSolverStatus IntVar CpSolverSolutionCallback LinearExpr]))
+
+(defn linear-distinct
+  [model linear-expr-seq]
+  (let [add-different (fn [left right]
+                        (.addDifferent model left right))]
+    (doseq [pair (combo/combinations linear-expr-seq 2)]
+      (apply add-different pair))))
+
+(defn solve-queens
+  [board-size]
+  (let [model (sat/new-model)
+        queens (mapv #(.newIntVar model 0 (dec board-size) (str "x" %)) (range board-size))
+        diag1 (for [i (range board-size)]
+                (LinearExpr/affine (queens i) 1 i))
+        diag2 (for [i (range board-size)]
+                (LinearExpr/affine (queens i) 1 (- i)))]
+    (.addAllDifferent model (into-array queens))
+    (linear-distinct model diag1)
+    (linear-distinct model diag2)
+    (sat/count-solutions model queens)))
+
+(defn queens-clojure
+  [n]
+  (filter (fn [x] (every? #(apply distinct? (map-indexed % x)) [+ -]))
+          (combo/permutations (range 1 (inc n)))))
+
+(comment
+  (time (solve-queens 11))
+  )
--- src/caesarhu/shun_tools/or_tools/sat.clj
+++ src/caesarhu/shun_tools/or_tools/sat.clj
@@ -4,16 +4,41 @@
 
 (Loader/loadNativeLibraries)
 
+(defn new-model
+  []
+  (CpModel.))
+
+(defn new-solver
+  []
+  (CpSolver.))
+
+(defn solve
+  [model vars]
+  (let [solver (new-solver)
+        status (.solve solver model)]
+    {:values (map #(.value solver %) vars) :status status}))
+
 (defn solutions
   [model vars]
   (let [counter (atom 0)
         values (atom [])
         cb (proxy [CpSolverSolutionCallback] []
              (onSolutionCallback []
-               (let [value (mapv #(.value this %) vars)]
-                 (swap! values conj value))
+               (swap! values conj (mapv #(.value this %) vars))
                (swap! counter inc)))
-        solver (CpSolver.)]
+        solver (new-solver)]
     (.setEnumerateAllSolutions (.getParameters solver) true)
     (let [status (.solve solver model cb)]
       {:count @counter :values @values :status status})))
+
+(defn count-solutions
+  [model vars]
+  (let [counter (atom 0)
+        cb (proxy [CpSolverSolutionCallback] []
+             (onSolutionCallback []
+               (swap! counter inc)))
+        solver (new-solver)]
+    (.setEnumerateAllSolutions (.getParameters solver) true)
+    (let [status (.solve solver model cb)]
+      {:count @counter :status status})))
+
